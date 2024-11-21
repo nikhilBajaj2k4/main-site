5d1dfb81e26f183fc16132d05f2ddf714fec51ac
nikhilBajaj2k4 <git@stackblitz.com>
2024-11-22 02:44:47 +0530

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F94949',
+        secondary: '#FFF4E0'
+      }
+    }
+  },
+  plugins: []
+}
